013d0d7675cbd461cad44246c4d44907a3623eec
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-04 18:19:48 +0100

    Ricevimento Dati e HTML ok

--- database.php
+++ database.php
@@ -6,4 +6,24 @@ if ($conn && $conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
     echo 'Errore di connessione ' . $conn->connect_error;
     die();
+}
+
+$sql = "SELECT * FROM `stanze`";
+$result = $conn->query($sql);
+// var_dump($result);
+
+if ($result && $result->num_rows > 0) { 
+    $rooms = [];
+    while($row = $result->fetch_assoc()) {
+        // echo "ID:" . $row['id'] . " " . $row['floor'];
+        $rooms[] = $row;
+      }
+    } 
+    elseif ($result) {
+        echo "No results"; 
+    } 
+    else {
+        echo "query error"; 
     }
+
+    $conn->close();
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+include __DIR__ .'/database.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,10 +8,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="dist/app.css">
-    <title>Document</title>
+    <title>Hotel-CRUD</title>
 </head>
 <body>
-    <h1>prova collegamento</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <h1>Stanze Disponibili</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Floor</th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if(!empty($rooms))
+            {
+                foreach ($rooms as $room) { ?>
+                <tr>
+                    <td><?php echo $room['id']?></td>
+                    <td><?php echo $room['floor']?></td>
+                    <td><a href="">VIEW</a></td>
+                    <td><a href="">UPDATE</a></td>
+                    <td>DELETE</td>
+                </tr>                   
+               <?php }
+            }
+            ?>
+            
+        </tbody>
+    </table>
+
+            </div>
+
+        </div>
+    </div>
+    
+
     
 </body>
 </html>
